6c990c302f1844ff59cf1c3d2c579ee897fe7766
zacha6 <zacharywhitehouse6@gmail.com>
2024-10-28 18:02:46 -0400

    finished AI API system

---
+++ api_gemini.py
@@ -0,0 +1,21 @@
+from dotenv import load_dotenv
+import os
+import google.generativeai as genai
+import time
+
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+
+def ai_response(user_input):
+    #Fetching the Gemini API
+    genai.configure(api_key=API_KEY)
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    response = model.generate_content(user_input).text
+
+    #splits response so it displays word after word
+    response_list = response.split(' ')
+
+    for word in response_list:
+        print(f"{word} ", end='', flush=True)
+        time.sleep(0.2)
